43fd4a4c9b5285e587c2affa60161b3a923f75fb
Tori <croteau.victoria1@gmail.com>
2021-03-01 12:02:40 -0500

    changed build settings, created my own scene and added placeholder img

---
+++ Team Prototype 1/Assets/Scripts/GunTouchTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunTouchTest : MonoBehaviour
+{
+
+    private Touch touch;
+    private Vector2 touchPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            //Instantiate()
+        }
+    }
+}
