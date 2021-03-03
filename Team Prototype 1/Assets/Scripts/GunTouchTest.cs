6d36f72a35f8675a7cb0d54082ff93733b9aef06
Tori <croteau.victoria1@gmail.com>
2021-03-03 12:21:36 -0500

    set up shooting mechanic with sfx and whatnot

--- Team Prototype 1/Assets/Scripts/GunTouchTest.cs
+++ Team Prototype 1/Assets/Scripts/GunTouchTest.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class GunTouchTest : MonoBehaviour
 {
 
-    private Touch touch;
+    private Vector3 touch;
     private Vector2 touchPosition;
+    AudioSource audioSource;
+    public GameObject bulletHole;
+    float width;
+    float height;
+    Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
+        for(int i = 0; i < Input.touchCount; i++)
         {
-            touch = Input.GetTouch(0);
-            //Instantiate()
+            if(Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                touch = Input.GetTouch(i).position;
+                audioSource.Play();
+                touch.z = Camera.main.nearClipPlane;
+                position = Camera.main.ScreenToWorldPoint (touch);
+
+                Instantiate(bulletHole, position, Quaternion.identity);
+                Debug.Log(position);
+            }
         }
     }
 }
