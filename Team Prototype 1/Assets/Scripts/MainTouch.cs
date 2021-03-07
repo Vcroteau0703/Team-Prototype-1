01a4f9eaf0478d0012d65f1541ea7b7c1a3c74b4
Tom Parker <parkertom076@gmail.com>
2021-03-07 15:32:46 -0500

    Flamethrower

--- Team Prototype 1/Assets/Scripts/MainTouch.cs
+++ Team Prototype 1/Assets/Scripts/MainTouch.cs
@@ -7,12 +7,14 @@ public class MainTouch : MonoBehaviour
     private Vector3 touch;
     private Vector2 touchPosition;
     AudioSource audioSource;
+    public GameObject flame;
     public GameObject bulletHole;
     public GameObject explosion;
     public Transform bulletHoleParent;
     Vector3 position;
     bool gun = false;
     bool explode = false;
+    bool flamethrower = false;
 
     // Start is called before the first frame update
     void Start()
@@ -43,18 +45,35 @@ public class MainTouch : MonoBehaviour
 
                 Instantiate(explosion, position, Quaternion.identity, bulletHoleParent);
             }
+            if (Input.GetTouch(i).phase == TouchPhase.Began && flamethrower)
+            {
+                //Torch the scene
+                touch = Input.GetTouch(i).position;
+                touch.z = Camera.main.nearClipPlane;
+                position = Camera.main.ScreenToWorldPoint(touch);
+
+                Instantiate(flame, position, Quaternion.identity, bulletHoleParent);
+            }
         }
     }
 
     public void ActivateGun()
     {
         explode = false;
+        flamethrower = false;
         gun = true;
     }
 
     public void ActivateNuke()
     {
         gun = false;
+        flamethrower = false;
         explode = true;
+    }
+    public void Activateflamethrower()
+    {
+        gun = false;
+        flamethrower = true;
+        explode = false;
     }
 }
