723cb0b5eac7c1cb38c0d51a1e0563ee37183221
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-03-07 22:41:41 -0500

    yikes

--- Team Prototype 1/Assets/Scripts/CutterTest.cs
+++ Team Prototype 1/Assets/Scripts/CutterTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutterTest : MonoBehaviour
 {
@@ -9,10 +10,10 @@ public class CutterTest : MonoBehaviour
     GameObject ad;
     public GameObject cutVersion; //The cut version of the ad
 
-    bool leftCut = false;
-    bool rightCut = false;
-    bool upCut = false;
-    bool bottomCut = false;
+    public bool leftCut = false;
+    public bool rightCut = false;
+    public bool upCut = false;
+    public bool bottomCut = false;
 
     [HideInInspector] public bool wasCut; //This will help set the Ad's Image Component back to "active" in it's own respective button script if this bool is true 
     bool cut = true;
@@ -22,30 +23,60 @@ public class CutterTest : MonoBehaviour
     Vector3 direction;
     public float moveSpeed;
 
+    Touch touch;
+
     private void Start()
     {
-        cutObject = GameObject.Find("Cutter");
-        cutColliders = new GameObject[4];
-        cutColliders[0] = GameObject.Find("Left Cut Collider");
-        cutColliders[1] = GameObject.Find("Right Cut Collider");
-        cutColliders[2] = GameObject.Find("Top Cut Collider");
-        cutColliders[3] = GameObject.Find("Bottom Cut Collider");  
+        ObjectAssignment();
+        wasCut = false;
     }
 
     private void Update()
     {
-        if(Input.touchCount > 0)
+        CutDebugErrors();
+
+        if(leftCut && rightCut)
         {
-            Touch touch = Input.GetTouch(0);
-            touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-            touchPosition.z = 0;
-            direction = (touchPosition - cutObject.transform.position);
-            
+            wasCut = true;
+            cutVersion = Resources.Load<GameObject>("Prefab Cut Versions/Jaden/Cut Jaden Horz");
+            Instantiate(cutVersion, cutObject.transform);
+
+            /*
+            if(ad.GetComponent<Image>().sprite == Resources.Load("transparentjaden"))
+            {
+                cutVersion = Resources.Load<GameObject>("Prefab Cut Versions/Jaden/Cut Jaden Horz");
+                Instantiate(cutVersion, cutObject.transform);
+            }
+            */
+        } 
+        else if(upCut && bottomCut)
+        {
+            wasCut = true;
+
+            if (ad.GetComponent<Image>().sprite == Resources.Load("transparentjaden"))
+            {
+                cutVersion = Resources.Load<GameObject>("Prefab Cut Versions/Jaden/Cut Jaden Vert");
+                Instantiate(cutVersion, ad.transform);
+            }
         }
 
+        switch (wasCut)
+        {
+            case true:
+                ad.GetComponent<Image>().enabled = false;
+                break;
+
+            case false:
+                ad.GetComponent<Image>().enabled = true;
+                break;
+        }
+        
+    }
 
 
 
+    void CutDebugErrors()
+    {
         //Debug Error Stuff I just wanted to do in case a scene doesn't have the following objects
         if (!GameObject.Find("Left Cut Collider"))
         {
@@ -67,5 +98,16 @@ public class CutterTest : MonoBehaviour
         {
             Debug.LogError("Please add the Cutter prefab for this to work");
         }
+    }
+
+    void ObjectAssignment()
+    {
+        ad = GameObject.Find("Ad");
+        cutObject = GameObject.Find("Cutter");
+        cutColliders = new GameObject[4];
+        cutColliders[0] = GameObject.Find("Left Cut Collider");
+        cutColliders[1] = GameObject.Find("Right Cut Collider");
+        cutColliders[2] = GameObject.Find("Top Cut Collider");
+        cutColliders[3] = GameObject.Find("Bottom Cut Collider");
     }
 }
